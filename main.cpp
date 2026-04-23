6bc1a548d07915cac5861a8984a8f0232628198c
aquilonorth <117199167+aquilonorth@users.noreply.github.com>
2026-04-22 19:29:15 -0500

    Update main.cpp
    changes to main for text file reading

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
-#include "Triangle.h"
+#include "Board.h"
+using namespace std;
 
 int main(){
 	Triangle t(1, 2);
-	std::cout << t.getColor() << " " << t.getOrientation() << '\n';
+	cout << t.getColor() << " " << t.getOrientation() << '\n';
+
+	string filename;
+	cin >> filename;
+	ifstream level(filename);
+
+	Triangle* levelBoard[29][10];
+	int parity = 1;
+	string colline;
+
+	for (int i = 0; i < 29; i++) { 
+
+		getline(level, colline);
+		for (int j = 0; j < 10; j++) {
+			string col = colline.substr(0, 1);
+			colline = colline.substr(2); //accounting for separator
+			int color = stoi(col);
+			levelBoard[i][j] = new Triangle(color, parity);
+		}
+		parity *= -1;
+	}
+
+	for (int i = 0; i < 29; i++) {
+		for (int j = 0; j < 10; j++) {
+			cout << levelBoard[i, j] << " ";
+		}
+		cout << endl;
+	}
+	
+	Board board = new Board(levelBoard);
 }
