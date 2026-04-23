b69acede0dc3501844497b57e91531b41f7bd524
Keyton <ton.of.keys@gmail.com>
2026-04-22 20:39:24 -0500

    Cmake I guess idfk

---
+++ build/CMakeFiles/KamiSolver.dir/Triangle.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/KamiSolver.dir/Triangle.cpp.obj: \
+ C:\Users\tonof\Documents\Code\Projects\KamiSolver\Triangle.cpp \
+ C:\Users\tonof\Documents\Code\Projects\KamiSolver\Triangle.h
--- main.cpp
+++ main.cpp
@@ -1,7 +1,56 @@
 #include <iostream>
 #include "Triangle.h"
+#include "Board.h"
+#include "raylib.h"
 
-int main(){
-	Triangle t(1, 2);
-	std::cout << t.getColor() << " " << t.getOrientation() << '\n';
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    int SCALE = 25;
+    const int screenWidth = 10 * SCALE;
+    const int screenHeight = 29 * SCALE;
+
+    Board board;
+    
+    
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            DrawTriangle((Vector2){ 0, 0 },
+                         (Vector2){ 0, 100 },
+                         (Vector2){ 100, 100 }, VIOLET);
+
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
 }
+
